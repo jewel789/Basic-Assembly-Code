a863ea1c04b7f323a7d3ad30107ad1d9d2cf2564
jewel789 <54429581+jewel789@users.noreply.github.com>
2020-02-26 10:24:58 +0600

    Stack introductory Code

---
+++ stack01.asm
@@ -0,0 +1,36 @@
+include 'emu8086.inc'
+.model small 
+.stack 200h
+.data 
+
+.code 
+
+main proc 
+    ;stack pointer (sp) - 0100h(address)
+    mov ax,'9'
+    mov bx,'2'   
+                       
+    push ax   ;ax(address)=100h and sp=sp-2h 
+    push bx   ;bx(address)=0FE  and sp=sp-2h
+    
+    pop cx    ;cx(address)=0FE  and sp=sp+2h
+    
+    mov ah,2
+    mov dx,cx
+    int 21h
+    
+    
+    
+    pop dx     ; dx(address)=0100h and sp+2h
+
+    mov ah,2
+    int 21h
+    
+    
+    main endp
+ end main
+    
+    
+    
+    
+    
